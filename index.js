53561ef2db1b7f02a12b3cad173251ff78a65227
ManivasagamSuresh <s.kishore123.64@gmail.com>
2023-02-09 18:36:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express  = require('express');
+const env  = require("dotenv").config();
+const cors = require("cors")
+const app = express();
+const nodemailer = require("nodemailer");
+const password = process.env.password;
+
+
+app.use(cors({
+    origin : "http://localhost:3000"
+}))
+
+app.use(express.json());
+
+
+
+app.post('/sendmail',async(req,res)=>{
+    const transporter =await nodemailer.createTransport(
+        {
+            service : "hotmail",
+            auth:{
+                user:"manivasagam.suresh@outlook.com",
+                pass:`${password}`
+            }
+        }
+    );
+    console.log("step1")
+    const details = {
+        from : "manivasagam.suresh@outlook.com",
+        to:'s.kishore123.64@gmail.com',
+        subject:"Hi there,i've checked your portfolio, do contact me",
+        text:`Name: ${req.body.name}
+              Email : ${req.body.email} 
+              My Message : ${req.body.message} `
+    };
+    console.log("step2")
+    await transporter.sendMail(details,(err,info)=>{
+        if(err){
+            console.log(err);
+            return
+        }
+        res.json(true);
+        console.log("sent:" + info.response)
+    })
+    
+})
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('connected')
+})
